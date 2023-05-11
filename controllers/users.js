14b0591cdfba75020e248f00fca3c2b505ba03b0
Anatoly Karpov <karpik-ne@yandex.ru>
2023-05-11 15:19:07 +0700

    ПР13: юзер готов

--- controllers/users.js
+++ controllers/users.js
@@ -3,5 +3,24 @@ const User = require('../models/user');
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
-  User.create({ name, about, avatar });
+  User.create({ name, about, avatar })
+    .then(user => res.send({ data: user }))
+    // если данные не записались, вернём ошибку
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+};
+
+module.exports.getAllUsers = (req, res) => {
+  User.find({})
+    .then((user) => {
+      res.send({ data: user });
+    })
+    .catch();
+};
+
+module.exports.getUserById = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .then((user) => {res.send({ data: user });
+    })
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
 };
--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const routerUsers = require('express').Router();
 
-const { createUser } = require('../controllers/users');
+const { createUser, getAllUsers, getUserById } = require('../controllers/users');
 
-routerUsers.post('/user', createUser);
 
-// routerUsers.get('/users');
+routerUsers.post('/users', createUser);
 
-// routerUsers.get('/users/me');
+routerUsers.get('/users', getAllUsers);
+
+routerUsers.get('/users/:userId', getUserById);
 
 module.exports = routerUsers;
