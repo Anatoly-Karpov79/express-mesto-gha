3017c25ba2cd5132889e47ab17a2feb3c2c8810a
Anatoly Karpov <karpik-ne@yandex.ru>
2023-05-21 20:54:25 +0700

    3

--- controllers/cards.js
+++ controllers/cards.js
@@ -34,35 +34,32 @@ module.exports.deleteCardById = (req, res) => {
     .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
 };
 
-module.exports.likeCard = (req, res ) => {
+module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
     { new: true },
   )
     .then((card) => {
-      if (!card) { res.status(400).send({ message: 'Неверный Id' });
+      if (!card) {
+        res.status(404).send({ message: 'Неверный Id' });
       }
       res.send({ data: card });
     })
     .catch(() => res.status(400).send({ message: 'Карточка не найдена' }));
 };
 
-module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $pull: { likes: req.user._id } }, // убрать _id из массива
-  { new: true },
-)
-  .then((card) => {
-    if (!card) {
-      throw new NotFoundError('Карточка не найдена');
-    }
-    res.send({ data: card });
-  })
-  .catch((err) => {
-    if (err.name === 'CastError') {
-      next(new BadRequestError('Некорректный Id'));
-      return;
-    }
-    next(err);
-  });
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) {
+        res.status(404).send({ message: 'Неверный Id' });
+      }
+      res.send({ data: card });
+    })
+    .catch(() => res.status(400).send({ message: 'Карточка не найдена' }));
+};
