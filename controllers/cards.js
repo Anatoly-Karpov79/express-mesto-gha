f71e5543620d27c39ea4c3f50a4ea3455e398932
Anatoly Karpov <karpik-ne@yandex.ru>
2023-05-21 20:48:30 +0700

    2

--- controllers/cards.js
+++ controllers/cards.js
@@ -10,7 +10,7 @@ module.exports.createCard = (req, res) => {
   Card.create({ name, link, owner })
     .then((cards) => res.send({ data: cards }))
     // если данные не записались, вернём ошибку
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }));
+    .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
 };
 
 module.exports.getAllCards = (req, res) => {
@@ -25,7 +25,7 @@ module.exports.deleteCardById = (req, res) => {
   const { cardId } = req.params;
   Card.findByIdAndRemove(cardId)
     .then((card) => {
-      if (!card) { throw new NotFoundError('Карточка не найдена'); }
+      if (!card) { res.status(404).send({ message: 'Неверный Id' }); }
     })
     .then(() => {
       res.send({ message: 'Карточка удалена' });
@@ -34,24 +34,19 @@ module.exports.deleteCardById = (req, res) => {
     .catch(() => res.status(400).send({ message: 'Произошла ошибка' }));
 };
 
-module.exports.likeCard = (req, res, next) => Card.findByIdAndUpdate(
-  req.params.cardId,
-  { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
-  { new: true },
-)
-  .then((card) => {
-    if (!card) {
-      throw new NotFoundError('Карточка не найдена');
-    }
-    res.send({ data: card });
-  })
-  .catch((err) => {
-    if (err.name === 'CastError') {
-      next(new BadRequestError('Некорректный Id'));
-      return;
-    }
-    next(err);
-  });
+module.exports.likeCard = (req, res ) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },
+  )
+    .then((card) => {
+      if (!card) { res.status(400).send({ message: 'Неверный Id' });
+      }
+      res.send({ data: card });
+    })
+    .catch(() => res.status(400).send({ message: 'Карточка не найдена' }));
+};
 
 module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
   req.params.cardId,
