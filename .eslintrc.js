d361a525b130df652027aff1953aaf291f7f427a
Anatoly Karpov <karpik-ne@yandex.ru>
2023-05-25 08:01:31 +0700

    ПР13: fix ревью 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,8 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'plugin:json/recommended',
     'airbnb-base',
   ],
   overrides: [
@@ -15,10 +11,6 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-
-  ],
   rules: {
     'no-underscore-dangle': ['error', { allow: ['_id'] }],
   },
